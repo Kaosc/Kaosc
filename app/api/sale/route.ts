022d29bb5f0defdec67766eb1458a40825ca3217
Kaosc <themelihbt@gmail.com>
2024-03-04 12:30:21 +0300

    update

---
+++ app/api/sale/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+import sale from "./sale.json"
+
+export async function GET(request: NextRequest) {
+	if (process.env.NODE_ENV !== "development") {
+		const authHeader = request.headers.get("authorization")
+		if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+			return new Response("Unauthorized Action", {
+				status: 401,
+			})
+		}
+	}
+
+	return NextResponse.json({ sale })
+}
